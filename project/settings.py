6686a5312b77d8487771a469ca954a157d75dc19
abhanya <abhanya01@gmail.com>
2022-11-18 12:50:14 +0530

    heroku run3

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u87loglnbra^dxa9w2#d45944-w&jj8(p#@&8lsgtdv@j2$c$x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
