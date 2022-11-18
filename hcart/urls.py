18e4c019a6eefa6521aeb144a54465812b104d1c
abhanya <abhanya01@gmail.com>
2022-11-18 12:36:20 +0530

    heroku run2

--- hcart/urls.py
+++ hcart/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('login',views.cart_login,name='clogin'),
     path('log',views.cart_log,name='login'),
     path('pass',views.cart_password,name='pass'),
-    path('',views.cust_home,name='home'),
+    path('home',views.cust_home,name='home'),
 ]
